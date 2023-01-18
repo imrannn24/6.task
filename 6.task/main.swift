4c6b6f4a7910259e54efcf663e76cff8e92f392d
imrannn24 <imran200424@icloud.com>
2023-01-18 23:06:25 +0600

    dz

--- 6.task/main.swift
+++ 6.task/main.swift
@@ -6,6 +6,121 @@
 //
 
 import Foundation
+//Д/З
+//Условие 1
+func calculator1(){
+    print("Введите первое число")
+    if let chislo1 = readLine(){
+        
+        let a =  Int(chislo1) ?? 0
+        print("Выберите действие: '+','-','*','/'")
+        
+        if let deistvie = readLine(){
+            
+            let c = String(deistvie)
+            
+            print("Введите второе число")
+            if let chislo2 = readLine(){
+                
+                let b = Int(chislo2) ?? 0
+                if c == "-"{
+                    print("\(a) - \(b) = \(a - b)")
+                }
+                else if c == "+"{
+                    print("\(a) + \(b) = \(a + b)")
+                }
+                else if c == "*"{
+                    print("\(a) * \(b) = \(a * b)")
+                }
+                else if c == "/"{
+                    print("\(a) / \(b) = \(a / b)")
+                }
+                else {
+                    print("error")
+                }
+            }
+        }
+        
+    }
+}
 
-print("Hello, World!")
+calculator1()
 
+//Условие 2
+func calculator2(){
+    print("Введите первое число")
+    var chislo1 = readLine()!
+    let a: Int? = Int(chislo1)
+    
+    print("Выберите действие: '+','-','*','/'")
+    var c = readLine()!
+    
+    print("Введите второе число")
+    var chislo2 = readLine()!
+    
+    let b: Int? = Int(chislo2)
+    
+    if a != nil && b != nil{
+        if c == "-"{
+            var d = a! - b!
+            print("\(a!) - \(b!) = \(d)")
+        }
+        else if c == "+"{
+            var d = a! + b!
+            print("\(a!) + \(b!) = \(d)")
+        }
+        else if c == "*"{
+            var d = a! * b!
+            print("\(a!) * \(b!) = \(d)")
+        }
+        else if c == "/"{
+            var d = a! / b!
+            print("\(a!) / \(b!) = \(d)")
+            
+        }else {
+            print("error")
+        }
+    }else{
+        print("Данные введены неверно")
+    }
+}
+calculator2()
+
+//Условие 3
+func calculator3(){
+    print("Введите первое число")
+    var chislo1 = readLine()!
+    let a: Int? = Int(chislo1)
+    
+    print("Выберите действие: '+','-','*','/'")
+    var c = readLine()!
+    
+    print("Введите второе число")
+    var chislo2 = readLine()!
+    
+    let b: Int? = Int(chislo2)
+    
+    guard a != nil && b != nil else{
+        print("Вы ввели неверные данные")
+        return}
+    if c == "-"{
+        let d = a! - b!
+        print("\(a!) - \(b!) = \(d)")
+    }
+    else if c == "+"{
+        let d = a! + b!
+        print("\(a!) + \(b!) = \(d)")
+    }
+    else if c == "*"{
+        let d = a! * b!
+        print("\(a!) * \(b!) = \(d)")
+    }
+    else if c == "/"{
+        let d = a! / b!
+        print("\(a!) / \(b!) = \(d)")
+        
+    }else {
+        print("error")
+    }
+}
+calculator3()
